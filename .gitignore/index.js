7b42b489ec9267bc483722bea9f136424dcb5177
Daehynah <43079514+Daehynah@users.noreply.github.com>
2018-09-08 00:07:38 +0200

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,411 @@
+const Discord = require('discord.js')
+const bot = new Discord.Client()
+
+bot.on('message', function (message) {
+
+
+	//Ambre
+	if ((message.content === '!buy Ambre') || (message.content === '!buy ambre') || (message.content === '!buy AMBRE')){
+
+		//Larimar
+		if (message.member.roles.has("486904294621052929")) {
+			message.member.removeRole("486904294621052929").catch(console.error)
+
+		//Emeraude
+		} else if (message.member.roles.has("486903533992673282")) {
+			message.member.removeRole("486903533992673282").catch(console.error)
+
+		//Or
+		} else if (message.member.roles.has("486904003976888320")) {
+			message.member.removeRole("486904003976888320").catch(console.error)
+
+		//Amethyste
+		} else if (message.member.roles.has("486903705237717003")) {
+			message.member.removeRole("486903705237717003").catch(console.error)
+
+		//Bonbon
+		} else if (message.member.roles.has("486903647855443979")) {
+			message.member.removeRole("486903647855443979").catch(console.error)
+
+		//Argent
+		} else if (message.member.roles.has("486903925719433237")) {
+			message.member.removeRole("486903925719433237").catch(console.error)
+
+		//Saphir
+		} else if (message.member.roles.has("486903357399891968")) {
+			message.member.removeRole("486903357399891968").catch(console.error)
+
+		//Diamant
+		} else if (message.member.roles.has("486904137976512542")) {
+			message.member.removeRole("486904137976512542").catch(console.error)
+
+		//Rubis
+		} else if (message.member.roles.has("486904462514978826")) {
+			message.member.removeRole("486904462514978826").catch(console.error)
+		}
+
+
+	//Amethyste
+	} else if ((message.content === '!buy Amethyste') || (message.content === '!buy amethyste') || (message.content === '!buy AMETHYSTE')){
+
+		//LArimar
+		if (message.member.roles.has("486904294621052929")) {
+			message.member.removeRole("486904294621052929").catch(console.error)
+
+		//Emeraude
+		} else if (message.member.roles.has("486903533992673282")) {
+			message.member.removeRole("486903533992673282").catch(console.error)
+
+		//Or
+		} else if (message.member.roles.has("486904003976888320")) {
+			message.member.removeRole("486904003976888320").catch(console.error)
+
+		//Ambre
+		} else if (message.member.roles.has("486903837974855681")) {
+			message.member.removeRole("486903837974855681").catch(console.error)
+
+		//Bonbon
+		} else if (message.member.roles.has("486903647855443979")) {
+			message.member.removeRole("486903647855443979").catch(console.error)
+
+		//Argent
+		} else if (message.member.roles.has("486903925719433237")) {
+			message.member.removeRole("486903925719433237").catch(console.error)
+
+		//Saphir
+		} else if (message.member.roles.has("486903357399891968")) {
+			message.member.removeRole("486903357399891968").catch(console.error)
+
+		//Diamant
+		} else if (message.member.roles.has("486904137976512542")) {
+			message.member.removeRole("486904137976512542").catch(console.error)
+
+		//Rubis
+		} else if (message.member.roles.has("486904462514978826")) {
+			message.member.removeRole("486904462514978826").catch(console.error)
+		}
+
+	//Argent
+	} else if ((message.content === '!buy Argent') || (message.content === '!buy argent') || (message.content === '!buy ARGENT')){
+
+		//Ambre
+		if (message.member.roles.has("486903837974855681")) {
+			message.member.removeRole("486903837974855681").catch(console.error)
+
+		//Amethyste
+		} else if (message.member.roles.has("486903705237717003")) {
+			message.member.removeRole("486903705237717003").catch(console.error)
+
+		//Bonbon
+		} else if (message.member.roles.has("486903647855443979")) {
+			message.member.removeRole("486903647855443979").catch(console.error)
+
+		//Diamant
+		} else if (message.member.roles.has("486904137976512542")) {
+			message.member.removeRole("486904137976512542").catch(console.error)
+
+		//Emeraude
+		} else if (message.member.roles.has("486903533992673282")) {
+			message.member.removeRole("486903533992673282").catch(console.error)
+
+		//Larimar
+		} else if (message.member.roles.has("486904294621052929")) {
+			message.member.removeRole("486904294621052929").catch(console.error)
+
+		//Or
+		} else if (message.member.roles.has("486904003976888320")) {
+			message.member.removeRole("486904003976888320").catch(console.error)
+
+		//Rubis
+		} else if (message.member.roles.has("486904462514978826")) {
+			message.member.removeRole("486904462514978826").catch(console.error)
+
+		//Saphir
+		} else if (message.member.roles.has("486903357399891968")) {
+			message.member.removeRole("486903357399891968").catch(console.error)
+		}
+
+	//Bonbon
+	} else if ((message.content === '!buy Bonbon') || (message.content === '!buy bonbon') || (message.content === '!buy BONBON')){
+
+		//Ambre
+		if (message.member.roles.has("486903837974855681")) {
+			message.member.removeRole("486903837974855681").catch(console.error)
+
+		//Amethyste
+		} else if (message.member.roles.has("486903705237717003")) {
+			message.member.removeRole("486903705237717003").catch(console.error)
+
+		//Argent
+		} else if (message.member.roles.has("486903925719433237")) {
+			message.member.removeRole("486903925719433237").catch(console.error)
+
+		//Diamant
+		} else if (message.member.roles.has("486904137976512542")) {
+			message.member.removeRole("486904137976512542").catch(console.error)
+
+		//Emeraude
+		} else if (message.member.roles.has("486903533992673282")) {
+			message.member.removeRole("486903533992673282").catch(console.error)
+
+		//LArimar
+		} else if (message.member.roles.has("486904294621052929")) {
+			message.member.removeRole("486904294621052929").catch(console.error)
+
+		//Or
+		} else if (message.member.roles.has("486904003976888320")) {
+			message.member.removeRole("486904003976888320").catch(console.error)
+
+		//Rubis
+		} else if (message.member.roles.has("486904462514978826")) {
+			message.member.removeRole("486904462514978826").catch(console.error)
+
+		//Saphir
+		} else if (message.member.roles.has("486903357399891968")) {
+			message.member.removeRole("486903357399891968").catch(console.error)
+		}
+
+	//Diamant
+	} else if ((message.content === '!buy Diamant') || (message.content === '!buy diamant') || (message.content === '!buy DIAMANT')){
+
+		//Ambre
+		if (message.member.roles.has("486903837974855681")) {
+			message.member.removeRole("486903837974855681").catch(console.error)
+
+		//Amethyste
+		} else if (message.member.roles.has("486903705237717003")) {
+			message.member.removeRole("486903705237717003").catch(console.error)
+
+		//Argent
+		} else if (message.member.roles.has("486903925719433237")) {
+			message.member.removeRole("486903925719433237").catch(console.error)
+
+		//Bonbon
+		} else if (message.member.roles.has("486903647855443979")) {
+			message.member.removeRole("486903647855443979").catch(console.error)
+
+		//Emeraude
+		} else if (message.member.roles.has("486903533992673282")) {
+			message.member.removeRole("486903533992673282").catch(console.error)
+
+		//Larimar
+		} else if (message.member.roles.has("486904294621052929")) {
+			message.member.removeRole("486904294621052929").catch(console.error)
+
+		//Or
+		} else if (message.member.roles.has("486904003976888320")) {
+			message.member.removeRole("486904003976888320").catch(console.error)
+
+		//Rubis
+		} else if (message.member.roles.has("486904462514978826")) {
+			message.member.removeRole("486904462514978826").catch(console.error)
+
+		//Saphir
+		} else if (message.member.roles.has("486903357399891968")) {
+			message.member.removeRole("486903357399891968").catch(console.error)
+		}
+
+	//Emeraude
+	} else if ((message.content === '!buy Emeraude') || (message.content === '!buy emeraude') || (message.content === '!buy EMERAUDE')){
+
+		//Amethyste
+		if (message.member.roles.has("486903705237717003")) {
+			message.member.removeRole("486903705237717003").catch(console.error)
+
+		//AMbre
+		} else if (message.member.roles.has("486903837974855681")) {
+			message.member.removeRole("486903837974855681").catch(console.error)
+
+		//Argent
+		} else if (message.member.roles.has("486903925719433237")) {
+			message.member.removeRole("486903925719433237").catch(console.error)
+
+		//Bonbon
+		} else if (message.member.roles.has("486903647855443979")) {
+			message.member.removeRole("486903647855443979").catch(console.error)
+
+		//Diamant
+		} else if (message.member.roles.has("486904137976512542")) {
+			message.member.removeRole("486904137976512542").catch(console.error)
+
+		//Larimar
+		} else if (message.member.roles.has("486904294621052929")) {
+			message.member.removeRole("486904294621052929").catch(console.error)
+
+		//Or
+		} else if (message.member.roles.has("486904003976888320")) {
+			message.member.removeRole("486904003976888320").catch(console.error)
+
+		//RUbis
+		} else if (message.member.roles.has("486904462514978826")) {
+			message.member.removeRole("486904462514978826").catch(console.error)
+
+		//Saphir
+		} else if (message.member.roles.has("486903357399891968")) {
+			message.member.removeRole("486903357399891968").catch(console.error)
+		}
+
+	//LArimar
+	} else if ((message.content === '!buy Larimar') || (message.content === '!buy larimar') || (message.content === '!buy LARIMAR')){
+
+		//AMbre
+		if (message.member.roles.has("486903837974855681")) {
+			message.member.removeRole("486903837974855681").catch(console.error)
+
+		//Amethyste
+		} else if (message.member.roles.has("486903705237717003")) {
+			message.member.removeRole("486903705237717003").catch(console.error)
+
+		//Argent
+		} else if (message.member.roles.has("486903925719433237")) {
+			message.member.removeRole("486903925719433237").catch(console.error)
+
+		//Bonbon
+		} else if (message.member.roles.has("486903647855443979")) {
+			message.member.removeRole("486903647855443979").catch(console.error)
+
+		//Diamant
+		} else if (message.member.roles.has("486904137976512542")) {
+			message.member.removeRole("486904137976512542").catch(console.error)
+
+		//Emeraude
+		} else if (message.member.roles.has("486903533992673282")) {
+			message.member.removeRole("486903533992673282").catch(console.error)
+
+		//Or
+		} else if (message.member.roles.has("486904003976888320")) {
+			message.member.removeRole("486904003976888320").catch(console.error)
+
+		//Rubis
+		} else if (message.member.roles.has("486904462514978826")) {
+			message.member.removeRole("486904462514978826").catch(console.error)
+
+		//Saphir
+		} else if (message.member.roles.has("486903357399891968")) {
+			message.member.removeRole("486903357399891968").catch(console.error)
+		}
+
+	//Or
+	} else if ((message.content === '!buy Or') || (message.content === '!buy or') || (message.content === '!buy OR')){
+
+		//Ambre
+		if (message.member.roles.has("486903837974855681")) {
+			message.member.removeRole("486903837974855681").catch(console.error)
+
+		//Amethyste
+		} else if (message.member.roles.has("486903705237717003")) {
+			message.member.removeRole("486903705237717003").catch(console.error)
+
+		//Argent
+		} else if (message.member.roles.has("486903925719433237")) {
+			message.member.removeRole("486903925719433237").catch(console.error)
+
+		//Bonbon
+		} else if (message.member.roles.has("486903647855443979")) {
+			message.member.removeRole("486903647855443979").catch(console.error)
+
+		//Diamant
+		} else if (message.member.roles.has("486904137976512542")) {
+			message.member.removeRole("486904137976512542").catch(console.error)
+
+		//Emeraude
+		} else if (message.member.roles.has("486903533992673282")) {
+			message.member.removeRole("486903533992673282").catch(console.error)
+
+		//LArimar
+		} else if (message.member.roles.has("486904294621052929")) {
+			message.member.removeRole("486904294621052929").catch(console.error)
+
+		//Rubis
+		} else if (message.member.roles.has("486904462514978826")) {
+			message.member.removeRole("486904462514978826").catch(console.error)
+
+		//Saphir
+		} else if (message.member.roles.has("486903357399891968")) {
+			message.member.removeRole("486903357399891968").catch(console.error)
+		}
+
+	//RUbis
+	} else if ((message.content === '!buy Rubis') || (message.content === '!buy rubis') || (message.content === '!buy RUBIS')){
+
+		//Ambre
+		if (message.member.roles.has("486903837974855681")) {
+			message.member.removeRole("486903837974855681").catch(console.error)
+
+		//Amethyste
+		} else if (message.member.roles.has("486903705237717003")) {
+			message.member.removeRole("486903705237717003").catch(console.error)
+
+		//Argent
+		} else if (message.member.roles.has("486903925719433237")) {
+			message.member.removeRole("486903925719433237").catch(console.error)
+
+		//Bonbon
+		} else if (message.member.roles.has("486903647855443979")) {
+			message.member.removeRole("486903647855443979").catch(console.error)
+
+		//Diamant
+		} else if (message.member.roles.has("486904137976512542")) {
+			message.member.removeRole("486904137976512542").catch(console.error)
+
+		//Emeraude
+		} else if (message.member.roles.has("486903533992673282")) {
+			message.member.removeRole("486903533992673282").catch(console.error)
+
+
+		//Larimar
+		} else if (message.member.roles.has("486904294621052929")) {
+			message.member.removeRole("486904294621052929").catch(console.error)
+
+		//Or
+		} else if (message.member.roles.has("486904003976888320")) {
+			message.member.removeRole("486904003976888320").catch(console.error)
+
+		//Saphir
+		} else if (message.member.roles.has("486904462514978826")) {
+			message.member.removeRole("486904462514978826").catch(console.error)
+		}
+
+	//Saphir
+	} else if ((message.content === '!buy Saphir') || (message.content === '!buy saphir') || (message.content === '!buy SAPHIR')){
+
+		//Ambre
+		if (message.member.roles.has("486903837974855681")) {
+			message.member.removeRole("486903837974855681").catch(console.error)
+
+		//Amethyste
+		} else if (message.member.roles.has("486903705237717003")) {
+			message.member.removeRole("486903705237717003").catch(console.error)
+
+		//Argent
+		} else if (message.member.roles.has("486903925719433237")) {
+			message.member.removeRole("486903925719433237").catch(console.error)
+
+		//Bonbon
+		} else if (message.member.roles.has("486903647855443979")) {
+			message.member.removeRole("486903647855443979").catch(console.error)
+
+		//Diamant
+		} else if (message.member.roles.has("486904137976512542")) {
+			message.member.removeRole("486904137976512542").catch(console.error)
+
+		//Emeraude
+		} else if (message.member.roles.has("486903533992673282")) {
+			message.member.removeRole("486903533992673282").catch(console.error)
+
+		//Larimar
+		} else if (message.member.roles.has("486904294621052929")) {
+			message.member.removeRole("486904294621052929").catch(console.error)
+
+		//Or
+		} else if (message.member.roles.has("486904003976888320")) {
+			message.member.removeRole("486904003976888320").catch(console.error)
+
+		//Rubis
+		} else if (message.member.roles.has("486904462514978826")) {
+			message.member.removeRole("486904462514978826").catch(console.error)
+		}
+	}
+}) 
+
+bot.login(process.env.TOKEN);
